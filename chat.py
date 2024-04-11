838c01f6b11c7083248a61ffa38abfc316673384
carlos <58930323+carloscao0928@users.noreply.github.com>
2024-04-11 22:42:41 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -3,57 +3,44 @@ import json
 import random
 import time
 
-def get_context(auth,chanel_id):
-
-    headr = {
+def get_random_message(channel_id, auth):
+    headers = {
         "Authorization": auth,
         "Content-Type": "application/json",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
     }
-    chanel_id = random.choice(chanel_list)
-    url = "https://discord.com/api/v9/channels/{}/messages?limit=100".format(chanel_id)
-    print(url)
-    res = requests.get(url=url, headers=headr)
-
+    url = f"https://discord.com/api/v9/channels/{channel_id}/messages?limit=100"
+    res = requests.get(url=url, headers=headers)
     result = json.loads(res.content)
-    result_list = []
-    for context in result:
-        if ('<') not in context['content'] :
-            if ('@') not in context['content'] :
-                if ('http') not in context['content']:
-                    if ('?') not in context['content']:
-                        result_list.append(context['content'])
 
-    return random.choice(result_list)
+    valid_messages = [msg['content'] for msg in result if not any(char in msg['content'] for char in ['<', '@', 'http', '?'])]
+    if valid_messages:
+        return random.choice(valid_messages)
+    else:
+        return None
 
-def chat(chanel_list,authorization):
-      header = {
-          "Authorization": authorization,
-          "Content-Type": "application/json",
-          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
-      }
-      for chanel_id in chanel_list:
-          msg = {
-              "content": "卷起来",
-              "nonce": "82329451214{}33232234".format(random.randrange(0, 1000000)),
-              "tts": False,
-          }
-          url = "https://discord.com/api/v9/channels/{}/messages".format(chanel_id)
-          try:
-              res = requests.post(url=url, headers=header, data=json.dumps(msg))
-              print(res.content)
-          except:
-              pass
-          continue
-      #time.sleep(random.randrange(10, 30))
+def send_message(channel_id, auth, message):
+    headers = {
+        "Authorization": auth,
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
+    }
+    url = f"https://discord.com/api/v9/channels/{channel_id}/messages"
+    data = {
+        "content": message
+    }
+    res = requests.post(url=url, headers=headers, data=json.dumps(data))
+    print(res.content)
 
-if __name__ == "__main__":
-    chanel_list = [""]
-    authorization_list = ""
+def chat(chanel_list, auth):
     while True:
-        try:
-            chat(chanel_list,authorization_list)
-            sleeptime = 61
-            time.sleep(sleeptime)
-        except:
-            break
+        for channel_id in chanel_list:
+            message = get_random_message(channel_id, auth)
+            if message:
+                send_message(channel_id, auth, message)
+                break
+        time.sleep(random.randrange(60, 61))
+
+chanel_list = ['']
+auth = ''
+chat(chanel_list, auth)
